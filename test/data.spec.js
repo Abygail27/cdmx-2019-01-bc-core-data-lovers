5dc377e8225b36e8654e3a927d9f80cdc6da79e4
Abygail27 <abygaailrrocha@gmail.com>
2019-02-18 13:26:42 -0600

    border a la tabla de porcentajes

--- test/data.spec.js
+++ test/data.spec.js
@@ -8,36 +8,14 @@ describe('dataBank', () => {
   })
 })
 
-describe('dataBank.MEX', () => {
-  it('MEX debería ser un objeto', ()=>{
-    expect(typeof dataBank).toBe('object');
-  })
-})
 
-describe('dataBank.PER',()=>{
-  it('PER debería ser un objeto', ()=>{
-    expect(typeof dataBank).toBe('object');
-  })
-})
-
-describe('dataBank.BRA',()=>{
-  it('BRA debería ser un objeto', ()=>{
-    expect(typeof dataBank).toBe('object');
-  })
-})
-describe('dataBank.CHL',()=>{
-  it('CHL debería ser un objeto',()=>{
-    expect(typeof dataBank).toBe('object');
-  })
-})
-
-describe('filter',()=>{
+describe('filterCountry',()=>{
   it('filter debería ser una función',()=>{
     expect(typeof window.dataBank.filter).toBe('function');
   })
 })
 
-describe('filterEdu',()=>{
+describe('filterIndicator',()=>{
   it('filterEdu debería ser una función',()=>{
     expect(typeof window.dataBank.filterEdu).toBe('function');
   })
